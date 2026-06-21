d62b274e2810a8f9bad85073c7b8b485ec27a4a1
angel <romeroangel933@gmail.com>
2026-06-21 18:00:38 -0300

    feat: codigo blink funcional y actualizacion de upload_port en platformio.ini

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,49 +1,14 @@
-#include <DHT.h>
-
-#define DHTPIN 15
-#define DHTTYPE DHT22
-#define GAS_PIN 2
-
-DHT dht(DHTPIN, DHTTYPE);
+#include <Arduino.h>
+#define LED_BUILTIN 2  
 
 void setup() {
-  Serial.begin(115200);
-
-  dht.begin();
-
-  pinMode(GAS_PIN, INPUT);
-
-  Serial.println("Smart Home Digital Twin Started");
+  // Configurar el pin del LED como salida
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-
-  int gasStatus = digitalRead(GAS_PIN);
-
-  Serial.println("----- Sensor Data -----");
-
-  if (!isnan(temperature) && !isnan(humidity)) {
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" °C");
-
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println(" %");
-  } else {
-    Serial.println("Failed to read DHT22!");
-  }
-
-  if (gasStatus == HIGH) {
-    Serial.println("Gas Detected!");
-  } else {
-    Serial.println("No Gas Detected");
-  }
-
-  Serial.println();
-
-  delay(2000);
+  digitalWrite(LED_BUILTIN, HIGH);   // Encender LED
+  delay(500);                        // Esperar 500 ms (0.5 segundos)
+  digitalWrite(LED_BUILTIN, LOW);    // Apagar LED
+  delay(500);                        // Esperar 500 ms
 }
