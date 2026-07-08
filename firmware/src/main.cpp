1141a19357e619b1749bdf71b83fbb2044cb6892
angel <romeroangel933@gmail.com>
2026-07-08 18:24:40 -0300

    Sector de pruebas unitarias por sensor y protocolo

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,49 +1,14 @@
-#include <DHT.h>
-
-#define DHTPIN 15
-#define DHTTYPE DHT22
-#define GAS_PIN 2
-
-DHT dht(DHTPIN, DHTTYPE);
-
+#include <Arduino.h>
+#define  LED_PIN 12
 void setup() {
-  Serial.begin(115200);
-
-  dht.begin();
-
-  pinMode(GAS_PIN, INPUT);
-
-  Serial.println("Smart Home Digital Twin Started");
+  // put your setup code here, to run once:
+  pinMode (LED_PIN, OUTPUT);
 }
 
 void loop() {
-
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-
-  int gasStatus = digitalRead(GAS_PIN);
-
-  Serial.println("----- Sensor Data -----");
-
-  if (!isnan(temperature) && !isnan(humidity)) {
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" °C");
-
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.println(" %");
-  } else {
-    Serial.println("Failed to read DHT22!");
-  }
-
-  if (gasStatus == HIGH) {
-    Serial.println("Gas Detected!");
-  } else {
-    Serial.println("No Gas Detected");
-  }
-
-  Serial.println();
-
-  delay(2000);
+  // put your main code here, to run repeatedly:
+  digitalWrite (LED_PIN, HIGH);	// turn on the LED
+  delay(500);	// wait for half a second or 500 milliseconds
+  digitalWrite (LED_PIN, LOW);	// turn off the LED
+  delay(500);	// wait for half a second or 500 milliseconds
 }
